61107bbd48ecbd90b4fc8d5da99bf67c21f6e1b9
avidworks <avidworks@live.com>
2022-02-21 15:11:28 -0500

    start ui

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+// game logic
 const ROUNDS_TO_PLAY = 5;
-let roundsPlayed = 0;
 let playerScore = 0;
 let computerScore = 0;
+
+// ui components
+const winnerDisplay = document.querySelector('#winner-display');
+const iconDisplay = document.querySelector('#icon');
+const playerIconDisplay = document.querySelector('#player-icon');
+const computerIconDisplay = document.querySelector('#computer-icon');
+
+// store ui icons in an object for easier rendering
+const icons = {
+  tied: `<i class="fa-solid fa-face-surprise"></i>`,
+  lost: `<i class="fa-solid fa-face-frown-open"></i>`,
+  won: `<i class="fa-solid fa-face-smile"></i>`,
+  gameWon: `<i class="fa-solid fa-face-laugh-beam"></i>`,
+  gameLost: `<i class="fa-solid fa-face-sad-cry"></i>`,
+  rock: `<i class="fa-solid fa-hand-back-fist"></i>`,
+  paper: `<i class="fa-solid fa-hand"></i>`,
+  scissors: `<i class="fa-solid fa-hand-scissors"></i>`,
+};
 
 // generate a random computer score
 const handleComputerChoice = () => {
@@ -15,10 +33,12 @@ const handleScore = (winner) => {
   winner === 'player' ? playerScore++ : computerScore++;
   console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
 
-  if (playerScore === 3) {
-    return 'You won the game!';
-  } else if (computerScore === 3) {
-    return 'You lost the game!';
+  if (playerScore === ROUNDS_TO_PLAY) {
+    winnerDisplay.innerText = 'You won the game!';
+    iconDisplay.innerHTML = icons.gameWon;
+  } else if (computerScore === ROUNDS_TO_PLAY) {
+    winnerDisplay.innerText = 'You lost the game!';
+    iconDisplay.innerHTML = icons.gameLost;
   }
 };
 
@@ -28,24 +48,36 @@ const playRound = (playerChoice, computerChoice) => {
     (playerChoice === 'paper' && computerChoice === 'rock') ||
     (playerChoice === 'scissors' && computerChoice === 'paper')
   ) {
-    roundsPlayed++;
+    iconDisplay.innerHTML = icons.won;
+    winnerDisplay.innerText = 'You won this round!';
     handleScore('player');
-    return 'You won this round!';
   } else if (playerChoice === computerChoice) {
-    return 'Tied!';
+    iconDisplay.innerHTML = icons.tied;
+    winnerDisplay.innerText = 'Tied!';
   } else {
-    roundsPlayed++;
+    iconDisplay.innerHTML = icons.lost;
+    winnerDisplay.innerText = 'You lost this round!';
     handleScore('computer');
-    return 'You lost this round!';
   }
 };
 
-const handleGame = () => {
-  const userChoice = prompt('Pick rock, paper or scissors');
+const handleGame = (userChoice) => {
   const computerChoice = handleComputerChoice();
+  playerIconDisplay.innerHTML = icons[userChoice];
+  computerIconDisplay.innerHTML = icons[computerChoice];
   playRound(userChoice, computerChoice);
 };
 
-while (roundsPlayed < ROUNDS_TO_PLAY) {
-  handleGame();
-}
+document.querySelector('#game-buttons').addEventListener('click', (e) => {
+  switch (e.target.getAttribute('id')) {
+    case 'rock':
+      handleGame('rock');
+      break;
+    case 'paper':
+      handleGame('paper');
+      break;
+    case 'scissors':
+      handleGame('scissors');
+      break;
+  }
+});
